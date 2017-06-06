34e0bb726e57f01a9a1b8b2cb6d7e975d3dda0e2
Danny Olson <dbolson@gmail.com>
2017-06-06 12:53:16 -0700

    Working on p03

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,17 @@
 package Scala99Problems
 
 object Main {
-  def foo(x: Int): Int = x + 1
+  def last[A](l: List[A]): A = l match {
+    case h :: Nil => h
+    case _ :: tail => last(tail)
+    case _ => throw new NoSuchElementException
+  }
+
+  def penultimate[A](l: List[A]): A = l match {
+    case p :: t :: Nil => p
+    case _ :: tail => penultimate(tail)
+    case _ => throw new NoSuchElementException
+  }
+
+  def nth[A](elem: A, l: List[A]): A = ???
 }
--- src/test/scala/MainTest.scala
+++ src/test/scala/MainTest.scala
@@ -4,7 +4,21 @@ import Scala99Problems.Main._
 
 class MainTest extends FlatSpec {
   "p01" should "find the last element of a list" in {
-    val result = foo(1)
+    val result = last(List(1, 1, 2, 3, 5, 8))
+    val expected = 8
+
+    assert(result == expected)
+  }
+
+  "p02" should "find the last but one element of a list" in {
+    val result = penultimate(List(1, 1, 2, 3, 5, 8))
+    val expected = 5
+
+    assert(result == expected)
+  }
+
+  "p03" should "find the Kth element of a list" in {
+    val result = nth(2, List(1, 1, 2, 3, 5, 8))
     val expected = 2
 
     assert(result == expected)
